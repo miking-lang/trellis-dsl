23f450143351cf8afa33727bb145ce093d816832
Lars Hummelgren <larshum@kth.se>
2024-02-13 00:08:09 +0100

    Add dishonest casino example

---
+++ examples/dishonest/make.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+case $1 in
+  build)
+    trellis casino.trellis
+    ;;
+  run)
+    python3 runner.py
+    ;;
+  clean)
+    rm -rf *generated* trellis.py __init__.py __pycache__ predecessors.txt
+    ;;
+  *)
+    >&2 echo "Expected argument 'build', 'run' or 'clean'"
+    exit 1
+    ;;
+esac
