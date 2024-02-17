da956554515a62231fae0df0e72331944c57e852
Lars Hummelgren <larshum@kth.se>
2024-02-17 18:45:46 +0100

    Make weather model slightly more interesting

--- examples/weather/runner.py
+++ examples/weather/runner.py
@@ -1,8 +1,16 @@
 import numpy as np
 from trellis import HMM
 
+# The observation probabilities when the weather is sunny is 0.8 (Happy) and
+# 0.2 (Grumpy).
+tables = {
+    'sunnyObs' : np.array([0.8, 0.2])
+}
+hmm = HMM(tables)
+
+# We observe only Happy, in which case the most likely sequence of states
+# should be only Sunny.
 signal = [0, 0, 0, 0, 0, 0, 0]
-hmm = HMM({})
 states = hmm.viterbi([signal])
 for s in states[0]:
     if s == 0:
