9218ebb00c66477220fd711e4fd0d8094094ac53
Lars Hummelgren <larshum@kth.se>
2024-05-08 15:37:04 +0200

    Remove files related to Futhark compilation

--- benchmarks/viterbi/trellis/run.py
+++ benchmarks/viterbi/trellis/run.py
@@ -11,6 +11,7 @@ if model_path:
     tables, signals = c.read_kmer_inputs_trellis(model_path, signals_path)
 else:
     tables, signals = c.get_weather_inputs_trellis(signals_path)
+
 hmm = HMM(tables)
 
 use_gpu = int(sys.argv[1])
@@ -29,11 +30,24 @@ elif k == 7:
 else:
     bsz = 100
 
-output = []
-t = 0.0
-for i in range(0, len(signals), bsz):
-    t0 = time.time()
-    output += hmm.viterbi(signals[i:i+bsz])
-    t1 = time.time()
-    t += t1-t0
-print(t)
+t0 = time.time()
+output = hmm.viterbi(signals, 5)
+t1 = time.time()
+t2 = time.time()
+p = hmm.forward(signals)
+t3 = time.time()
+
+outc = "ACGT"
+for i, seq in enumerate(output):
+    print(f"Seq #{i+1} ({p[i]})")
+    col = 0
+    for s in seq:
+        if s // 16384 == 0:
+            if col == 80:
+                print("")
+                col = 0
+            col += 1
+            print(f"{outc[s % 4]}", end="")
+    print("")
+print(t1-t0)
+print(t3-t2)
