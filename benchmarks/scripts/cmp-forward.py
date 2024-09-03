185d277dc19b3fa9c039b226a51c1a53bca9477d
Lars Hummelgren <larshum@kth.se>
2024-09-03 10:23:51 +0200

    Add simple scripts for comparing output data

---
+++ benchmarks/scripts/cmp-forward.py
@@ -0,0 +1,63 @@
+# Script for comparing the probabilities computed by the Forward algorithm for
+# different tools. It computes the arithmetic mean and the standard deviation
+# of the absolute difference in the probabilities reported by each pair of
+# results.
+
+import statistics
+import sys
+
+
+def read_output_file(o):
+    with open(o, "r") as f:
+        return [float(line) for line in f.readlines()]
+
+
+def compare_probs(l1, l2):
+    diffs = [abs(p1-p2) for p1, p2 in zip(l1, l2)]
+    return sum(diffs), statistics.mean(diffs), statistics.stdev(diffs)
+
+
+def run_comparison(probs, labels):
+    for i in range(len(probs)):
+        for j in range(i+1, len(probs)):
+            print(f"Comparing {labels[i]} and {labels[j]}")
+            assert len(probs[i]) == len(probs[j])
+            sum, avg, stddev = compare_probs(probs[i], probs[j])
+            print(f"{avg} ± {stddev} (total: {sum})")
+
+
+def default_files(model):
+    return [
+        f"out/pc-sparse-{model}-forward-data.txt",
+        f"out/pg-sparse-{model}-forward-data.txt",
+        f"out/pc-dense-{model}-forward-data.txt",
+        f"out/pg-dense-{model}-forward-data.txt",
+        f"out/z-{model}-forward-data.txt",
+        f"out/tc-{model}-forward-data.txt",
+    ]
+
+# By default, if no arguments are provided, the script automatically compares
+# the benchmark outputs for both the weather and the 3mer model.
+if len(sys.argv) == 1:
+    labels = [
+        "Pomegranate Sparse CPU",
+        "Pomegranate Sparse GPU",
+        "Pomegranate Dense CPU",
+        "Pomegranate Dense GPU",
+        "ZipHMM",
+        "Trellis"
+    ]
+    weather_files = default_files("weather")
+    kmer_files = default_files("3mer")
+
+    print("Weather model:")
+    weather_data = [read_output_file(o) for o in weather_files]
+    run_comparison(weather_data, labels)
+    print("="*20)
+    print("Kmer model:")
+    kmer_data = [read_output_file(o) for o in kmer_files]
+    run_comparison(kmer_data, labels)
+else:
+    data = [read_output_file(o) for o in sys.argv[1:]]
+    labels = sys.argv[1:]
+    run_comparison(data, labels)
---
+++ benchmarks/scripts/cmp-viterbi.py
@@ -0,0 +1,71 @@
+# Script for comparing the output of the Viterbi algorithm from different
+# tools. It computes a similarity score using the Smith-Waterman algorithm for
+# each pair of the provided outputs - a positive score indicates the results
+# are very similar.
+
+import sys
+from Bio.Align import PairwiseAligner
+from Levenshtein import distance
+
+
+def read_state_seq(arg):
+    try:
+        with open(arg) as f:
+            # Only compare the first 10k characters per instance to prevent
+            # excessive slowdowns when ran on very long sequences.
+            return [str(line.strip())[:10000] for line in f.readlines()]
+    except:
+        # Produce an empty sequence, so that the comparison function knows to
+        # skip this version and its corresponding label.
+        return []
+
+
+def run_comparison(seqs, labels):
+    comp = [(label, seq) for label, seq in zip(labels, seqs) if len(seq) > 0]
+    for i in range(len(comp)):
+        l1, s1 = comp[i]
+        for j in range(i+1, len(comp)):
+            l2, s2 = comp[j]
+            print(f"Comparing {l1} and {l2}")
+            assert len(s1) == len(s2)
+            dists = [distance(a, b) for a, b in zip(s1, s2)]
+            print(sum(dists))
+
+
+def bench_output_files(model):
+    return [
+        f"out/s-{model}-viterbi-data.txt",
+        f"out/n-{model}-viterbi-data.txt",
+        f"out/tc-{model}-viterbi-data.txt",
+        f"out/tr-{model}-viterbi-data.txt",
+    ]
+
+# If no arguments are provided, we compare the (interpreted) results produced
+# by running the Viterbi algorithm of different tools on the weather and 3mer
+# model.
+if len(sys.argv) == 1:
+    labels = [
+        "StochHMM",
+        "Native CUDA",
+        "Trellis (compile-time)",
+        "Trellis (runtime)",
+    ]
+    weather_files = bench_output_files("weather")
+    kmer_files1 = bench_output_files("3mer-nobatch")
+    kmer_files2 = bench_output_files("3mer-batch")
+
+    print("Weather model:")
+    weather_data = [read_state_seq(o) for o in weather_files]
+    run_comparison(weather_data, labels)
+    print("="*20)
+    print("Kmer model (no batching):")
+    kmer_data = [read_state_seq(o) for o in kmer_files1]
+    run_comparison(kmer_data, labels)
+    print("="*20)
+    print("Kmer model (batching):")
+    kmer_data = [read_state_seq(o) for o in kmer_files2]
+    run_comparison(kmer_data, labels)
+else:
+    labels = sys.argv[1:]
+    data = [read_state_seq(l) for l in labels]
+    run_comparison(data, labels)
